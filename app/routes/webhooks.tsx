a91440f39c0ef95caa24e71134680f00e36f7724
Nick Wesselman <27013789+nickwesselman@users.noreply.github.com>
2024-09-04 13:16:15 -0400

    webhook for tagging customer

--- app/routes/webhooks.tsx
+++ app/routes/webhooks.tsx
@@ -1,10 +1,11 @@
 import type { ActionFunctionArgs } from "@remix-run/node";
 import { authenticate } from "../shopify.server";
 import db from "../db.server";
+import { AdminApiContext, Session } from "@shopify/shopify-app-remix/server";
 
 export const action = async ({ request }: ActionFunctionArgs) => {
   const { topic, shop, session, admin, payload } = await authenticate.webhook(request);
-  console.log("Webhook received", { topic, payload });
+  console.log("Webhook received", topic, JSON.stringify(payload, null, 2));
 
   if (!admin && topic !== 'SHOP_REDACT') {
     // The admin context isn't returned if the webhook fired after a shop was uninstalled.
@@ -23,7 +24,9 @@ export const action = async ({ request }: ActionFunctionArgs) => {
 
       break;
     case "ORDERS_CREATE":
-      console.log("Order created", payload);
+      if (admin) {
+        await tagCustomerIfTheyBoughtMembership(payload, admin);
+      }
       break;
     case "CUSTOMERS_DATA_REQUEST":
     case "CUSTOMERS_REDACT":
@@ -34,3 +37,35 @@ export const action = async ({ request }: ActionFunctionArgs) => {
 
   throw new Response();
 };
+
+async function tagCustomerIfTheyBoughtMembership(payload: Record<string, any>, admin: AdminApiContext) {
+  if (!payload.line_items.find((lineItem: { sku: string; }) => lineItem.sku === 'MEMBERSHIP')) {
+    console.log("No membership product found in order");
+    return;
+  }
+
+  const customerId = payload.customer.admin_graphql_api_id;
+  console.log("Tagging customer as member:", customerId);
+
+  const response = await admin.graphql(`#graphql
+    mutation addTags($id: ID!, $tags: [String!]!) {
+      tagsAdd(id: $id, tags: $tags) {
+        node {
+          id
+        }
+        userErrors {
+          message
+        }
+      }
+    }
+  `, {
+    variables: {
+      id: customerId,
+      tags: ["member"]
+    }
+  });
+  const responseJson = await response.json();
+  if (responseJson.data?.tagsAdd?.userErrors?.length) {
+    console.error("Failed to tag customer as member", responseJson.data.addTags.userErrors);
+  }
+}
