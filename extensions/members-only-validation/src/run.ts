d6a8d119c8a7765155171440528918b0c597ff6f
Nick Wesselman <27013789+nickwesselman@users.noreply.github.com>
2024-09-04 17:50:56 -0400

    update function to not error if membership is in cart

--- extensions/members-only-validation/src/run.ts
+++ extensions/members-only-validation/src/run.ts
@@ -9,30 +9,54 @@ export function run(input: RunInput): FunctionRunResult {
     errors: [],
   };
 
+  // Don't show errors in the cart
   if (input.buyerJourney.step === BuyerJourneyStep.CartInteraction) {
     return noErrors;
   }
 
-  if (input.cart.buyerIdentity?.isAuthenticated && input.cart.buyerIdentity?.customer?.isMember?.jsonValue) {
+  // If they are logged in and a member, they can purchase anything
+  const isAuthenticated = input.cart.buyerIdentity?.isAuthenticated;
+  if (isAuthenticated && input.cart.buyerIdentity?.customer?.isMember?.jsonValue) {
     return noErrors;
   }
 
+  // Determine if the cart contains a members-only product or membership
+  let membersOnlyProduct = null;
+  let cartContainsMembership = false;
   for (const line of input.cart.lines) {
-    if (line.merchandise.__typename === "ProductVariant" && line.merchandise.product.isMemberOnly?.jsonValue
-    ) {
-      const message = input.cart.buyerIdentity?.isAuthenticated ?
-        `You must be a member to purchase ${line.merchandise.product.title}. Please purchase a membership.` :
-        `You must be a logged-in member to purchase ${line.merchandise.product.title}. Please log in to validate your membership.`;
-
-      return {
-        errors: [
-          {
-            localizedMessage: message,
-            target: "$.cart",
-          },
-        ],
-      };
+    if (line.merchandise.__typename !== "ProductVariant") {
+      continue;
     }
+
+    if (line.merchandise.product.isMemberOnly?.jsonValue) {
+      membersOnlyProduct = line.merchandise.product;
+    } else if (line.merchandise.sku === "MEMBERSHIP") {
+      cartContainsMembership = true;
+    }
+  }
+
+  // They need to log in
+  if (membersOnlyProduct && !isAuthenticated) {
+    return {
+      errors: [
+        {
+          localizedMessage: `You must be a logged-in member to purchase ${membersOnlyProduct.title}. Please log in to validate your membership.`,
+          target: "$.cart",
+        },
+      ],
+    };
+  }
+
+  // They need to purchase a membership
+  if (membersOnlyProduct && !cartContainsMembership) {
+    return {
+      errors: [
+        {
+          localizedMessage: `You must be a member to purchase ${membersOnlyProduct.title}. Please purchase a membership.`,
+          target: "$.cart",
+        },
+      ],
+    };
   }
 
   return noErrors;
